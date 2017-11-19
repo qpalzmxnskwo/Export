add4af773839b6fc34ed16d64f9b150df9a8c9ec
srututututu <ola.stolorz@vp.pl>
2017-11-20 00:57:14 +0100

    merge naprawiony

--- send_mail.php
+++ send_mail.php
@@ -10,29 +10,21 @@ use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
 
-<<<<<<< HEAD
-$text= $_POST['message'];
-$export_data=$_SESSION['array'];
-$export_data = json_decode(json_encode($export_data), true);
-$num=1;
 
-send_mail($text, $export_data, $num);
-
-
-
-function send_mail($text, $export_data, $num){
-=======
 $text=$_POST['subject'];
 
 $tmp_name= $_FILES['filesToUpload']['tmp_name'];
 $name= $_FILES['filesToUpload']['name'];
 
-send_mail($text, $tmp_name, $name);
->>>>>>> 4695a2ae00372ff648c49346ec1a281a19b6ad95
+$export_data=$_SESSION['array'];
+$export_data = json_decode(json_encode($export_data), true);
 
 
+$num=1; 
 
-function send_mail($text, $tmp_name, $name){
+send_mail($text, $tmp_name, $name, $export_data, $num);
+
+function send_mail($text, $tmp_name, $name, $export_data, $num){
 
 $mail = new PHPMailer();
 $mail->SMTPOptions = array(
@@ -49,7 +41,7 @@ $mail->Host = 'poczta.interia.pl';
 $mail->Port = 587;
 $mail->SMTPSecure = 'tls';
 $mail->SMTPAuth = true;
-<<<<<<< HEAD
+
 $mail->Username = "mailtestowy74@interia.pl";
 $mail->Password = "haslo12";
 $mail->setFrom('mailtestowy74@interia.pl');
@@ -65,25 +57,13 @@ $email_list[]=$val['Email'];
 }
  $shifted = array_shift($email_list);
 }
-=======
-$mail->Username = "";
-$mail->Password = "";
-$mail->setFrom('');
 
-
- $i=0;
- while ($i <= 1) {
-$mail->addAddress();
-	 $i++;
- }
->>>>>>> 4695a2ae00372ff648c49346ec1a281a19b6ad95
-
-	$mail->addAddress($shifted);
+$mail->addAddress($shifted);
    
 	
-
 $mail->Subject = 'PHPMailer GMail SMTP test';
 $mail->Body = $text;
+
 foreach (array_keys($tmp_name) as $key){
 $mail->AddAttachment($tmp_name[$key],$name[$key]);
 }
@@ -94,29 +74,11 @@ if (!$mail->send()) {
     if (!empty($email_list)) {
     	var_dump($email_list);
     	sleep(10);
-    	send_mail($text, $shifted, 2);
+    	send_mail($text,$tmp_name, $name, $shifted, 2);
     }
 }
 }
 
 
-
-<<<<<<< HEAD
-
-
-
-
-
-
-
-
 ?>
 
-
-
-
-
-
-=======
-?>
->>>>>>> 4695a2ae00372ff648c49346ec1a281a19b6ad95
