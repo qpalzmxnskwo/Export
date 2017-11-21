98a0d30a51e3e94a2eba3a2610ee7a45fa361a39
srututututu <ola.stolorz@vp.pl>
2017-11-21 13:28:41 +0100

    nowy send_mail

--- send_mail.php
+++ send_mail.php
@@ -1,4 +1,6 @@
+
 <?php
+
 set_time_limit(0);
 
 session_start();
@@ -30,11 +32,7 @@ send_mails($email_list, $text, $tmp_name,$name);
 
 function send_mails($email_list, $text, $tmp_name,$name){
 if (!empty($email_list)) {
-<<<<<<< HEAD
-	for($i=0; $i<10; $i++){
-=======
 	for($i=0; $i<5; $i++){
->>>>>>> 2b8982186c2726a59c1e017ca1312b7bf1d5b457
 	$email = array_shift($email_list);	
 	echo$ii;
 	$ii++;}
@@ -58,24 +56,14 @@ $mail->SMTPOptions = array(
 
 $mail->isSMTP();
 $mail->SMTPDebug = 2;
-$mail->Host = 'poczta.interia.pl';
+$mail->Host = 'smtp.gmail.com';
 $mail->Port = 587;
 $mail->SMTPSecure = 'tls';
 $mail->SMTPAuth = true;
-<<<<<<< HEAD
-$mail->Username = "mailtestowy75@interia.pl";
-$mail->Password = "haslo12345";
-$mail->setFrom('mailtestowy75@interia.pl');
-$mail->AddCC($email);
-} 
-
-
-=======
 $mail->Username = "troleklolek123123@gmail.com";
 $mail->Password = "troleklolek321";
 $mail->setFrom('troleklolek123123@gmail.com');
 $mail->AddCC($email);
->>>>>>> 2b8982186c2726a59c1e017ca1312b7bf1d5b457
 $mail->Subject = 'PHPMailer GMail SMTP test';
 $mail->Body = $text;
 
@@ -85,15 +73,10 @@ $mail->AddAttachment($tmp_name[$key],$name[$key]);
 
 if (!$mail->send()) {
     echo "Mailer Error: " . $mail->ErrorInfo;
-<<<<<<< HEAD
-} else {	
-    echo 'ok';
-=======
 } else {
     echo 'wysłano';
->>>>>>> 2b8982186c2726a59c1e017ca1312b7bf1d5b457
 }
-
+}
 
 
 ?>
